44a660764d184c85ab4e764dd1957fcb956617c5
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-23 14:11:13 -0300

    ArraysUnidimensionais

---
+++ src/ArraysUnidimensionais.java
@@ -0,0 +1,16 @@
+
+public class ArraysUnidimensionais {
+		public void Exemplo2() {
+			
+			/* Array (vetor) de inteiros, com tamanho igual 50 */
+			int numeros[] = new int[50];
+			
+			/* Laço 'for' que preenche o vetor com valores de 0 a 50
+			 * sempre aumentando de 10 em 10 unidades
+			*/
+			for (int i = 0; i <= numeros.length; i++) {
+				numeros[i] = i * 10;
+			}
+		}
+	
+}
