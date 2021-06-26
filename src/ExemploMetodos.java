3db3e2d2cd2d0a47d4e9749bd4b65f4ac16e3acb
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-26 09:26:58 -0300

    Oficina_Métodos

---
+++ src/ExemploMetodos.java
@@ -0,0 +1,21 @@
+
+public class ExemploMetodos {
+  public static void somar(double valor_1,double valor_2) {
+   
+  }
+  public static void subtrair(double valor_1,double valor_2) {
+	  
+  }
+  public static void multiplicar(double valor_1,double valor_2) {
+	  
+  }
+  public static void dividir(double valor_1,double valor_2) {
+	  
+  }
+  public static void main(String[] args) {
+	 somar(2.9, 4.7);
+	 subtrair(3.0, 1.5);
+	 multiplicar(2.0, 3.0);
+	 dividir(6.0, 2.0);
+  }
+}
