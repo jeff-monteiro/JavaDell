68fcddf89751ebae50e9009c225fb5a52f20aff7
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-28 20:01:12 -0300

    ExemploArraysMultiD

---
+++ src/ExemploArraysMulti.java
@@ -0,0 +1,23 @@
+
+public class ExemploArraysMulti {
+	  public static void somar(double valor_1, double valor_2){
+		
+	  }
+	  public static void subtrair(double valor_1, double valor_2){
+		  
+	  }
+	  public static void multiplicar(double valor_1, double valor_2){
+		  
+	  }
+	  public static void dividir(double valor_1, double valor_2){
+		 
+	  }
+	  public static void main(String[] args){
+	    System.out.println(somar(2.9, 4.7));
+	    System.out.println(subtrair(3.0, 1.5));
+	    System.out.println(multiplicar(2.0, 3.0));
+	    System.out.println(dividir(6.0, 2.0));
+	  }
+	  
+ }
+
