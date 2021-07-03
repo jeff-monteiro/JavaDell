19f2ad9ee766a4b9d23bd8a63245c92aae31b1de
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-03 16:38:36 -0300

    POO-01

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,31 @@
 
-public class Pessoa {
+public class Pessoa { //Classe Pessoa.
+	public String nome;//Atributos da classe Pessoa.
+	public int idade;
 	
+	public void andar() {//Ações ou comportamentos da classe.
+		System.out.println("Estou caminhando");
+	}
+	public void falar() {
+		System.out.println("bla bla bla");
+	}
+	public void apresentar() {
+		System.out.println("Olá, meu nome é: " + nome);
+	}
+	
+	public class TestePessoa{
+		public void main(String[]args) {
+			/*Criando um objeto para representar José de 32 anos*/
+			Pessoa jose = new Pessoa();
+			jose.nome = "José";
+			
+			/*Criando um objeto para representar Lara de 25 anos*/
+			Pessoa lara = new Pessoa();
+			lara.nome = "Lara";
+			
+			/*Fazendo ambos os objetos executar o método apresetar()*/
+			jose.apresentar();
+			lara.apresentar();
+		}
+	}
 }
