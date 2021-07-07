18b9deddf795156b3435e753cea974e9400205f0
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-06 22:21:08 -0300

    Pacotes

--- src/modelos/Aluno.java
+++ src/modelos/Aluno.java
@@ -6,8 +6,27 @@ public class Aluno {
 	double nota2;
 	
 	public Aluno(String nome, double nota1, double nota2) {
-		this.nome = "Jefferson";
-		this.nota1 = 7.1;
-		this.nota2 = 9.0;
+		this.nome = nome;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota2() {
+		return nota2;
 	}
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,7 +1,18 @@
 package principal;
 
+import modelos.Aluno;
+
 public class Principal {
 	public static void main(String[]args) {
-		
+		Aluno aluno = new Aluno("Pedro", 8.75, 6.25);
+		System.out.println(aluno.getNome());
+		System.out.println(aluno.getNota1());
+		System.out.println(aluno.getNota2());
+		System.out.println(calcularMedia(aluno.getNota1(), aluno.getNota2()));
 	}
+	public static double calcularMedia(double nota1, double nota2) {
+		return(nota1 + nota2)/2;	
+	}
+	
+	
 }
