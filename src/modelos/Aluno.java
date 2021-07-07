d25090f4ccf4595b56c2e2ccfffca3b7b78a470a
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-06 22:29:49 -0300

    Pacotes

--- src/modelos/Aluno.java
+++ src/modelos/Aluno.java
@@ -1,16 +1,16 @@
 package modelos;
 
-public class Aluno {
-	String nome;
+public class Aluno {//Clase Aluno
+	String nome; //Atributos da classe
 	double nota1;
 	double nota2;
 	
-	public Aluno(String nome, double nota1, double nota2) {
+	public Aluno(String nome, double nota1, double nota2) {//Método construtor
 		this.nome = nome;
 		this.nota1 = nota1;
 		this.nota2 = nota2;
 	}
-
+	//Métodos Getters & Setters
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -4,14 +4,14 @@ import modelos.Aluno;
 
 public class Principal {
 	public static void main(String[]args) {
-		Aluno aluno = new Aluno("Pedro", 8.75, 6.25);
-		System.out.println(aluno.getNome());
+		Aluno aluno = new Aluno("Pedro", 8.75, 6.25);//Instânciação da classe Aluno
+		System.out.println(aluno.getNome());//Impressão dos atributos do objeto aluno
 		System.out.println(aluno.getNota1());
 		System.out.println(aluno.getNota2());
 		System.out.println(calcularMedia(aluno.getNota1(), aluno.getNota2()));
 	}
-	public static double calcularMedia(double nota1, double nota2) {
-		return(nota1 + nota2)/2;	
+	public static double calcularMedia(double nota1, double nota2) {//Método calcularMedia
+		return(nota1 + nota2)/2;//Imprimir resultado do calculo da Média Aritmética.	
 	}
 	
 	
