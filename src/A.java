bc84b499f51fa8d058973ebdd671cdf4ab842b81
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-09 16:11:12 -0300

    ExemploSuperClasse

---
+++ src/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+	public int valor;
+	public A() {//Construtor
+		this.valor = 0;
+	}
+	public int calcularValor() {//Método
+		return valor;
+	}
+	
+}
---
+++ src/C.java
@@ -0,0 +1,9 @@
+
+public class C extends A{
+		private int bonus;
+		public C (int valor, int bonus) {
+			super();
+			this.bonus = bonus;
+		}
+			
+}
