b8f12ca78d9b36b16464607fe4c0579d010e777e
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-14 22:16:17 -0300

    Herança

---
+++ src/Auto/Teste.java
@@ -0,0 +1,12 @@
+package Auto;
+
+public class Teste {
+	public static void main(String[]args) {
+		Carro carro = new Carro();
+		
+		carro.setPeso(500);
+		carro.setCor("Amarelo");
+		carro.setPreco(35000);
+		carro.setQuantidadeDePortas(4);
+	}
+}
