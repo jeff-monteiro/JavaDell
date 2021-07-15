a52c662d78d73335ea30986c1be3dc2eacd504ab
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-14 21:57:59 -0300

    Herança

--- src/Auto/Carro.java
+++ src/Auto/Carro.java
@@ -1,6 +1,17 @@
 package Auto;
 
 public class Carro extends Automovel{
+	int quantidadeDePortas;
 	
+	public void setQuantidadeDePortas(int quantidadeDePortas) {
+		this.quantidadeDePortas = quantidadeDePortas;
+	}
+	public int getQuantidadeDePortas() {
+		return quantidadeDePortas;
+	}
+	public void exibir() {
+		super.exibir();
+		System.out.println("Quantidade de Portas: "+ this.getQuantidadeDePortas());
+	}
 
 }
