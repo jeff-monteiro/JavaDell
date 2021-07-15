4d53145754848cf851a6e7f5c4319fe135d64f36
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-14 21:27:55 -0300

    Herança

--- src/Auto/Automovel.java
+++ src/Auto/Automovel.java
@@ -11,5 +11,16 @@ public class Automovel {
 	public float getPeso() {
 		return peso;
 	}
-	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public float getPreco() {
+		return preco;
+	}
 }
