689405fad97a9bda509ef4a1226055e92cfe4611
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-15 23:10:19 -0300

    Herança

--- src/Auto/Teste.java
+++ src/Auto/Teste.java
@@ -2,11 +2,12 @@ package Auto;
 
 public class Teste {
 	public static void main(String[]args) {
-		Carro carro = new Carro();
-		
+		Carro carro = new Carro();//Método Construtor.
+		//Definição dos valores utilizando o método SET.
 		carro.setPeso(500);
 		carro.setCor("Amarelo");
 		carro.setPreco(35000);
 		carro.setQuantidadeDePortas(4);
+		carro.exibir();
 	}
 }
---
+++ src/recursoshumanos/Funcionario.java
@@ -0,0 +1,29 @@
+package recursoshumanos;
+
+public class Funcionario {
+	  public long cpf;
+	  public double salario;
+	  public double desconto;
+	  
+	  public void setCpf(long cpf){
+	   this.cpf = cpf;
+	  }
+	  public long getCpf(){
+	   return cpf;
+	  }
+	  public void setSalario(double salario){
+	   this.salario = salario;
+	  }
+	  public double getSalario(){
+	   return salario;
+	  }
+	  public void setDesconto(double desconto){
+	   this.desconto = desconto;
+	  }
+	  public double getDesconto(){
+	   return desconto;
+	  }
+	  public static void main(String[]args){
+	     
+	  }
+}
