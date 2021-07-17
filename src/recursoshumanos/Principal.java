36518eda10f3721788a7f2142aa7887810c172bf
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-17 00:47:33 -0300

    Herança

---
+++ src/recursoshumanos/Principal.java
@@ -0,0 +1,12 @@
+package recursoshumanos;
+
+public class Principal {
+	public static void main(String[]args) {
+		Funcionario funcionario = new Funcionario();
+	    funcionario.setCpf(12345678901);
+	    funcionario.setSalario(1209.75);
+	    funcionario.setDesconto(102.35);
+	    
+	    
+	}
+}
