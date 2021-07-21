d85ebee6fcb29356c5e992a33d8e08786ce91623
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-21 15:13:11 -0300

    ClassesAbstratas

---
+++ src/aula06/Pessoa.java
@@ -0,0 +1,11 @@
+package aula06;
+
+public class Pessoa {
+	private String nome;
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	public String toString() {
+		return nome;
+	}
+}
---
+++ src/aula06/Professor.java
@@ -0,0 +1,14 @@
+package aula06;
+
+public class Professor extends Pessoa{
+	private String disciplina;
+	private String[] turmas;
+	
+	public Professor(String nome, String disciplina) {
+		super(nome);
+		this.disciplina = disciplina;
+	}
+	public String toString() {
+		return super.toString() + "Jefferson" + "\n Portuguese" + disciplina;
+	}
+}
