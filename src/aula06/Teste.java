ff6d3d40dcec870a05e104ab37425cdb20182b5a
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-21 16:03:55 -0300

    ClassesAbstratas

--- src/aula06/Teste.java
+++ src/aula06/Teste.java
@@ -8,5 +8,15 @@ public class Teste {
 		System.out.println(professor);
 		System.out.println(aluno);
 	}
+	 
+//	public class Teste {
+//		  public static void main(String[] args){ 
+//		  Pessoa professor = new Professor("João", "Matemática");
+//		  Pessoa aluno = new Aluno("Rafael", "124873223");
+//			*NUNCA UTILIZAR DESSA FORMA*
+//		    Pessoa pessoa = new Pessoa("Claudio");
+//		    System.out.println("pessoa");
+//		  }
+//		}
 	
 }
