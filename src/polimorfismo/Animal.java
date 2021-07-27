6cb9986090b6fee741830afc7d9d86a789f28971
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-27 20:36:47 -0300

    Polimorfismo

--- src/polimorfismo/Animal.java
+++ src/polimorfismo/Animal.java
@@ -1,6 +1,8 @@
 package polimorfismo;
 
-public class Animal {
+/* As classes Cavalo, Sapo, Serumano são subclasses a partir da SuperClasse Animal*/
+
+public class Animal {//SuperClasse
 	public void moverSe(int qtAndar) {
 		System.out.println("Mover-se");
 	}
---
+++ src/polimorfismo/TestesAnimais.java
@@ -0,0 +1,18 @@
+package polimorfismo;
+
+public class TestesAnimais {
+	public static void main(String[]args) {
+		//Instanciando um objeto a partir de uma classe
+		Animal a1 = new Animal();
+		
+		//instanciando objetos de subclasses para referências de superclasse
+		Animal a2 = new Animal();
+		Animal a3 = new Animal();
+		Animal a4 = new Animal();
+		
+		a1.moverSe(2);
+		a2.moverSe(2);
+		a3.moverSe(2);
+		a4.moverSe(2);
+	}
+}
