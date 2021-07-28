b1a1be4ae9396319a77c9fe521ca6dab7548cd19
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-07-27 21:01:09 -0300

    Polimorfismo

--- src/polimorfismo/TestesAnimais.java
+++ src/polimorfismo/TestesAnimais.java
@@ -2,17 +2,36 @@ package polimorfismo;
 
 public class TestesAnimais {
 	public static void main(String[]args) {
-		//Instanciando um objeto a partir de uma classe
-		Animal a1 = new Animal();
-		
-		//Instanciando objetos de subclasses para referências de superclasse
-		Animal a2 = new Cavalo();
-		Animal a3 = new Sapo();
-		Animal a4 = new SerHumano();
-		
-		a1.moverSe(2);
-		a2.moverSe(2);
-		a3.moverSe(2);
-		a4.moverSe(2);
+		//Definição de referência de superclasse
+		Animal a;
+		System.out.println("1 - Ser Humano \n2 - Cavalo \n3 - Sapo" );
+		int opc = 1;
+		System.out.println("Opção escolhida " + opc);
+		//Instanciando objeto de uma das subclasses para referência da superclasse
+		if(opc == 1) 
+			a = new SerHumano();
+		else	
+			if(opc == 2)
+				a = new Cavalo();
+			else
+				if(opc == 3)
+					a = new Sapo();
+				else
+					a = new Animal();
+				
+			
 	}
-}
+}	
+		
+		//Instanciando um objeto a partir de uma classe
+//		Animal a1 = new Animal();
+//		
+//		//Instanciando objetos de subclasses para referências de superclasse
+//		Animal a2 = new Cavalo();
+//		Animal a3 = new Sapo();
+//		Animal a4 = new SerHumano();
+//		
+//		a1.moverSe(2);
+//		a2.moverSe(2);
+//		a3.moverSe(2);
+//		a4.moverSe(2);
