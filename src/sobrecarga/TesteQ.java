b2967291564c7bb42ddd88f42a44d815a3279dc4
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-08-02 19:56:54 -0300

    SobrecargaJava

--- src/sobrecarga/TesteQ.java
+++ src/sobrecarga/TesteQ.java
@@ -1,7 +1,7 @@
 package sobrecarga;
 
 public class TesteQ {
- public static void main(String[]args) {
+ public static void main(String[]args) {//Teste de Branch
 	 Quadrado q1 = new Quadrado();
 	 Quadrado q2 = new Quadrado(10, 15);
 	 Quadrado q3 = new Quadrado(20, 30, 50);
