24c7ca921cb0fb7d851bae650fb2919f90649976
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-08-01 23:38:16 -0300

    SobrecargaJava

--- src/sobrecarga/ExemploSobrecarga.java
+++ src/sobrecarga/ExemploSobrecarga.java
@@ -5,4 +5,10 @@ public class ExemploSobrecarga {
 		if(v1 > v2) return (v1);
 		else return (v2);
 	}
+	public int maiorValor(int v1, int v2, int v3) {
+		int maiorTemp = v1;
+		if(v2 > maiorTemp) maiorTemp = v2;
+		if(v3 > maiorTemp) maiorTemp = v3;
+		 return (maiorTemp);
+	}
 }
