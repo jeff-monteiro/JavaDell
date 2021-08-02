d83fb56703cc637ba8ee52de4bd34da2d9996f1b
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-08-01 23:12:24 -0300

    SobrecargaJava

--- src/sobrecarga/ExemploComissionado.java
+++ src/sobrecarga/ExemploComissionado.java
@@ -1,5 +1,10 @@
 package sobrecarga;
 
 public class ExemploComissionado {
-
+	private double comissaoPorPeca;
+	private int quantidadePecasVendidas;
+	
+	public double valorDoSalario() {
+		return super.valorDoSalario() + comissaoPorPeca * quantidadePecasVendidas;
+	}
 }
